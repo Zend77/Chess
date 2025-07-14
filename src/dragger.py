44d34668133bdcb34c42ae7bc94663ddff62db75
Zend77 <clarkbadams@gmail.com>
2025-07-14 05:05:40 -0500

    Refactor Dragger class: add type hints and clean up code formatting

--- src/dragger.py
+++ src/dragger.py
@@ -1,46 +1,45 @@
 import pygame as p
+from typing import Optional, Any
 
 from const import *
 
 
 class Dragger:
-    
+    piece: Optional[Any]
+    mouseX: int
+    mouseY: int
+    initial_row: int
+    initial_col: int
+    dragging: bool
+
     def __init__(self):
-        self.piece = None
-        self.mouseX = 0
-        self.mouseY = 0
-        self.initial_row = 0
-        self.initial_col = 0
-        self.dragging = False
-        
-    # Blit method
-        
-    def update_blit(self, surface, theme_name=None):
-        # Texture
+        self.piece: Optional[Any] = None
+        self.mouseX: int = 0
+        self.mouseY: int = 0
+        self.initial_row: int = 0
+        self.initial_col: int = 0
+        self.dragging: bool = False
+
+    def update_blit(self, surface, theme_name: Optional[str]=None) -> None:
         self.piece.set_texture(size=128, theme_name=theme_name) # type: ignore
         texture = self.piece.texture # type: ignore
-        # Img
         img = p.image.load(texture)
-        # Rect
         img_center = (self.mouseX, self.mouseY)
         self.piece.texture_rect = img.get_rect(center=img_center) # type: ignore
-        # Blit
         surface.blit(img, self.piece.texture_rect) # type: ignore
-        
-    # Other methods
-    
-    def update_mouse(self, pos):
-        self.mouseX, self.mouseY = pos # (xcord, ycord)
-        
-    def save_inital(self, pos):
+
+    def update_mouse(self, pos: tuple[int, int]) -> None:
+        self.mouseX, self.mouseY = pos
+
+    def save_inital(self, pos: tuple[int, int]) -> None:
         self.initial_row = pos[1] // SQ_SIZE
         self.initial_col = pos[0] // SQ_SIZE
-        
-    def drag_piece(self, piece):
+
+    def drag_piece(self, piece: Any) -> None:
         self.piece = piece
         self.dragging = True
-    
-    def undrag_piece(self, theme_name=None):
+
+    def undrag_piece(self, theme_name: Optional[str]=None) -> None:
         if self.piece:
             self.piece.set_texture(size=80, theme_name=theme_name)
         self.piece = None
