5b6b2d37617c6fe0e646eae28a249bb94ba20ab0
Zend77 <clarkbadams@gmail.com>
2025-07-14 05:06:10 -0500

    Refactor Move class: add type hints and improve method signatures for clarity

--- src/move.py
+++ src/move.py
@@ -1,15 +1,19 @@
+from typing import Any
 
 class Move:
-    def __init__(self, initial, final):
+    def __init__(self, initial: Any, final: Any):
         # initial and final are squares
         self.initial = initial
         self.final = final 
-    
-    def __str__(self):
+
+    def __str__(self) -> str:
         s = ''
         s += f'({self.initial.col}, {self.initial.row})'
         s += f'({self.final.col}, {self.final.row})'
         return s
-    
-    def __eq__(self, other):
-        return self.initial == other.initial and self.final == other.final
+
+    def __eq__(self, other: object) -> bool:
+        return (
+            isinstance(other, Move) and
+            self.initial == other.initial and self.final == other.final
+        )
